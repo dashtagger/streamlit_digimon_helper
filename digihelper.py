8c4a5f180fc69d470ac2ce7a71cd70d85209dc5c
jianwei <jianweilee1092@gmail.com>
2023-03-30 01:55:37 +0800

    update bt-10,minor improvements

--- digihelper.py
+++ digihelper.py
@@ -11,13 +11,37 @@ id_check_tab, advanced_tab = st.tabs(["ID", "Advanced"])
 
 
 with id_check_tab:
-    search_IDs=st.text_input("Input ID")
+    search_IDs=st.text_input("Input ID").replace(" ", "")
     st.write("example: bt1-001,st1-01")
     search_ID_list=list(map(str.lower,search_IDs.split(',')))
 
     if search_IDs != '':
         selected_df=df[df["ID Code"].apply(lower).isin(search_ID_list)]
         st.write(selected_df)
+
+        for index, row in selected_df.iterrows():
+            #Color	Card Name	Stage	Level	Rarity	DP	Play Cost	DigiCost	Main Effect	Inherited Effect	Security	Attribute	Type
+
+            st.header(row['Card Name'])
+            left, right = st.columns(2)
+            left.write("Color 1: " + str(row['Color 1']))
+            left.write("Color 2: " + str(row['Color 2']))
+            left.write("Type: " + str(row['Type']))
+            left.write("Stage: " + str(row['Stage']))
+            left.write("Attribute: " + str(row['Attribute']))
+            right.write("DigiCost 1: " + str(row['DigiCost 1']))
+            right.write("DigiCost 2: " + str(row['DigiCost 2']))
+            right.write("Level: " + str(row['Level']))
+            right.write("DP: " + str(row['DP']))
+            right.write("Play Cost: " + str(row['Play Cost']))
+            st.subheader("Main Effect")
+            st.write(row['Main Effect'])
+            st.subheader("Inherited Effect")
+            st.write(row['Inherited Effect'])
+            st.subheader("Security")
+            st.write(row['Security'])
+            if pd.notna(row["Url"]):
+                st.image(row['Url'])
 
 with advanced_tab:
     search_names=st.multiselect("Select Card Names",options=set(df['Card Name'].tolist()))
